27c8995d1159e494b7319ec0032b9bac5534eed8
athos araujo <athosaraujo@Mini-de-athos.local>
2023-08-10 20:54:43 -0300

    8 primeiros endpoints concluidos

--- src/controladores/usuarios.js
+++ src/controladores/usuarios.js
@@ -5,13 +5,13 @@ const senha_jwt = require('../senha_jwt');
 
 const cadastrarUsuario = async (req, res) => {
     const { nome, email, senha } = req.body;
-    if(!nome || !email || !senha){
+    if (!nome || !email || !senha) {
         return res.status(400).json({ mensagem: "Todos os campos são obrigatorios: nome, email e senha " });
     }
 
     const arrayEmail = email.split("");
-   
-    if(!arrayEmail.includes("@")) {
+
+    if (!arrayEmail.includes("@")) {
         return res.status(400).json({ mensagem: "Email em formato invalido" });
     }
 
@@ -24,12 +24,12 @@ const cadastrarUsuario = async (req, res) => {
 
         const verificarEmail = await pool.query(queryEmail, [email]);
 
-        if(verificarEmail.rowCount > 0) {
+        if (verificarEmail.rowCount > 0) {
             return res.status(400).json({ mensagem: "Email ja cadastrado" });
         }
 
         const senhaCriptografada = await bcrypt.hash(senha, 10);
-        
+
         const queryInsert = (`
             INSERT INTO usuarios
                 (nome, email, senha)
@@ -38,7 +38,7 @@ const cadastrarUsuario = async (req, res) => {
         `);
 
         const cadastrarUsuario = await pool.query(queryInsert, [nome, email, senhaCriptografada]);
-        
+
         return res.status(201).json(cadastrarUsuario.rows[0]);
     } catch (error) {
         return res.status(500).json({ mensagem: "Erro interno do servidor" });
@@ -47,10 +47,10 @@ const cadastrarUsuario = async (req, res) => {
 
 const logarUsuario = async (req, res) => {
     const { email, senha } = req.body;
-    if(!email || !senha){
+    if (!email || !senha) {
         return res.status(404).json({ mensagem: "Todos os campos são obrigatorios: nome, email e senha " });
     }
-    
+
     try {
         const queryEmail = (`
             SELECT *
@@ -58,13 +58,13 @@ const logarUsuario = async (req, res) => {
             WHERE email = $1
         `);
         const verificarEmail = await pool.query(queryEmail, [email]);
-        if(verificarEmail.rowCount == 0) {
+        if (verificarEmail.rowCount == 0) {
             return res.status(400).json({ mensagem: "Email Ou Senha Invalidos" });
         }
 
         const senhaValida = await bcrypt.compare(senha, verificarEmail.rows[0].senha);
 
-        if(!senhaValida) {
+        if (!senhaValida) {
             return res.status(400).json({ mensagem: "Email Ou Senha Invalidos" });
         }
 
@@ -78,7 +78,7 @@ const logarUsuario = async (req, res) => {
             }
         );
 
-        const {senha:_, ...usuarioSemSenha } = verificarEmail.rows[0];
+        const { senha: _, ...usuarioSemSenha } = verificarEmail.rows[0];
 
         return res.status(200).json({
             usuario: usuarioSemSenha, token
@@ -92,22 +92,139 @@ const obterUsuario = async (req, res) => {
     const id = req.usuario;
     // Consultar usuário no banco de dados pelo ID contido no TOKEN informado \\
     try {
-        const queryObeterUsuario = await pool.query (`
+        const queryObeterUsuario = await pool.query(`
             SELECT *
             FROM usuarios
             WHERE id = $1`,
             [id]
         );
-    const { senha:_, ...usuarioSemSenha } = queryObeterUsuario.rows[0];
+        const { senha: _, ...usuarioSemSenha } = queryObeterUsuario.rows[0];
 
-    return res.status(200).json(usuarioSemSenha);
+        return res.status(200).json(usuarioSemSenha);
     } catch (error) {
         return res.status(500).json({ mensagem: "Erro interno do servidor" });
     }
 }
 
+const atualizarUsuario = async (req, res) => {
+    const id = req.usuario;
+    const { nome, email, senha } = req.body;
+    if (!nome || !email || !senha) {
+        return res.status(400).json({ mensagem: "Todos os campos são obrigatorios: nome, email e senha " });
+
+    }
+    try {
+        const queryEmail = (`
+            SELECT *
+            FROM usuarios
+            WHERE email = $1
+        `);
+
+        const verificarEmail = await pool.query(queryEmail, [email]);
+
+        if (verificarEmail.rowCount > 0) {
+            return res.status(400).json({ mensagem: "Email ja cadastrado" });
+        }
+
+
+        const senhaCriptografada = await bcrypt.hash(senha, 10);
+
+
+        const queryUpdate = (`
+            UPDATE usuarios set nome = $1, email=$2, senha = $3 where id = $4
+        `);
+
+        const atualizarUsuario = await pool.query(queryUpdate, [nome, email, senhaCriptografada, id]);
+
+        return res.status(204).json({});;
+    } catch (error) {
+        return res.status(404).json({ mensagem: "Erro interno do servidor" });
+    }
+
+}
+
+const cadastrarCategoria = async (req, res) => {
+    const { descricao } = req.body;
+    if (!descricao) {
+        return res.status(400).json({ "mensagem": "O campo descricão é obrigatório." });
+    }
+    const id = req.usuario;
+    try {
+        const queryCadastrarDescricao = (`
+        INSERT into categorias (usuario_id,descricao) values ($1,$2) returning id,descricao,usuario_id`
+        );
+
+        const cadastrarCategoria = await pool.query(queryCadastrarDescricao, [id, descricao]);
+        return res.status(201).json(cadastrarCategoria.rows[0]);
+    } catch (error) {
+        return res.status(500).json({ "mensagem": "Erro interno do servidor." });
+    }
+}
+
+const detalharCategoria = async (req, res) => {
+    const { id } = req.params;
+    const idAutenticado = req.usuario;
+    try {
+        const queryDetalharCategoria = (`
+        SELECT * from categorias where id = $1 and usuario_id = $2
+        `);
+        const detalharCategoria = await pool.query(queryDetalharCategoria, [id, idAutenticado]);
+        if (detalharCategoria.rowCount < 1) {
+            return res.status(404).json({ "mensagem": "categoria não existente" })
+        }
+        return res.status(200).json(detalharCategoria.rows[0]);
+
+    } catch (error) {
+        return res.status(500).json({ "mensagem": "erro interno de servidor" })
+    }
+
+
+}
+
+const listarCategorias = async (req, res) => {
+    const idAutenticado = req.usuario;
+    try {
+        const queryDetalharCategoria = (`
+        SELECT * from categorias where usuario_id = $1
+        `);
+        const listarCategoria = await pool.query(queryDetalharCategoria, [idAutenticado]);
+        return res.status(200).json(listarCategoria.rows);
+
+    } catch (error) {
+        return res.status(500).json({ "mensagem": "erro interno de servidor" })
+    }
+}
+
+const deletarCategoria = async (req, res) => {
+
+    const { id } = req.params;
+    const idAutenticado = req.usuario;
+
+    try {
+        const queryBuscarCategoria = (`
+        SELECT * from categorias where id = $1 and usuario_id = $2
+        `);
+
+        const buscarCategoria = await pool.query(queryBuscarCategoria, [id, idAutenticado]);
+        if (buscarCategoria.rowCount < 1) {
+            return res.status(404).json({ "mensagem": "categoria não encontrada." })
+        }
+
+
+        const deletarCategoria = await pool.query('delete from categorias where id = $1', [id]);
+        return res.status(204).json({});
+    } catch (error) {
+        return res.status(500).json({ "mensagem": "erro interno de servidor." })
+    }
+
+}
 module.exports = {
     cadastrarUsuario,
     logarUsuario,
-    obterUsuario
+    obterUsuario,
+    atualizarUsuario,
+    cadastrarCategoria,
+    detalharCategoria,
+    listarCategorias,
+    deletarCategoria
 }
--- src/rotas.js
+++ src/rotas.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { cadastrarUsuario, logarUsuario, obterUsuario } = require('./controladores/usuarios');
+const { cadastrarUsuario, logarUsuario, obterUsuario, atualizarUsuario, cadastrarCategoria, detalharCategoria, listarCategorias, deletarCategoria } = require('./controladores/usuarios');
 const { verificarUsuarioLogado } = require('./intermediarios/autenticacao');
 const rotas = express();
 
@@ -8,6 +8,13 @@ rotas.post('/login', logarUsuario);
 
 rotas.use(verificarUsuarioLogado);
 
-rotas.get('/usuario', obterUsuario)
+rotas.get('/usuario', obterUsuario);
+rotas.put('/usuario', atualizarUsuario);
+
+
+rotas.post('/categoria', cadastrarCategoria);
+rotas.get('/categoria/:id', detalharCategoria);
+rotas.get('/categoria', listarCategorias);
+rotas.delete('/categoria/:id', deletarCategoria);
 
 module.exports = rotas;
