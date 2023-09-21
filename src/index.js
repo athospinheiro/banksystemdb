346ecba879c8bbe8dd2287a97479cb327cced07c
aeikus <aeikus@outlook.com>
2023-09-21 19:58:37 -0300

    node

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const rotas = require('./rotas');
 const app = express();
-
+require('dotenv').config();
+const cors = require('cors');
+app.use(cors());
 app.use(express.json());
 app.use(rotas);
 
-app.listen(3000);
+const port = process.env.PORT;
+app.listen(port, ()=>{
+    console.log(port);
+});
